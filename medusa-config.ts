a45fa142bf77d343820f946881806c055d3aaec7
anthonyhuanggr <anthonyhuanggr@gmail>
2025-11-03 15:31:26 -0500

    +++

--- medusa-config.ts
+++ medusa-config.ts
@@ -10,8 +10,12 @@ module.exports = defineConfig({
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+      jwtSecret:
+        process.env.JWT_SECRET ||
+        "51f263c68b801b100ee197c70941f4acd5ba0e69693e4fe84f42138da812faa7",
+      cookieSecret:
+        process.env.COOKIE_SECRET ||
+        "d79564029fa3b180e00e5346a61f752b8815d9f46c9bbdd208d9161f49685430",
     },
   },
 });
